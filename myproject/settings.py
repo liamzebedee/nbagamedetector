41602f20ae06d3d1d283a7b721be9090f1f2b7e7
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2016-09-05 00:13:46 +1000

    asd

--- myproject/settings.py
+++ myproject/settings.py
@@ -136,6 +136,12 @@ STATICFILES_DIRS = (
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
 
 # List of finder classes that know how to find static files in
 # various locations.
