ac23678d5b921e9c99777cf6dcab255a806539cc
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2016-09-04 18:04:50 +1000

    download games done

--- downloadGames.py
+++ downloadGames.py
@@ -1,20 +1,23 @@
 from datetime import date
 from dateutil.rrule import rrule, DAILY
+import re
+import urllib2
 
-startDate = date(2015, 10, 27)
-endDate = date(2016, 6, 19)
+start_of_season = date(2015, 10, 27)
+end_of_season = date(2016, 6, 19)
 
-def getGameUrl(date):
+def get_game_url(date):
 	return "http://cdn.espn.go.com/core/nba/schedule/_/date/"+date+"?table=true&device=desktop&edition-host=espn.com.au&userab=0"
 
-for dt in rrule(DAILY, dtstart=startDate, until=endDate):
-	datestr = dt.strftime("%Y%m%d")
-	print(getGameUrl(datestr))
+pattern = r"\/nba\/game\?gameId\=([0-9]+)"
+regex = re.compile(pattern, re.IGNORECASE)
 
-# \/nba\/game\?gameId\=[0-9]+
-# strip "/nba/game?gameId="
+for date in rrule(DAILY, dtstart=start_of_season, until=end_of_season):
+	date_str = date.strftime("%Y%m%d")
 
+	game_details_html = urllib2.urlopen(get_game_url(date_str)).read()
 
+	for match in regex.finditer(game_details_html):
+		game_id = match.group(1)
 
-
-# open('')
+		
