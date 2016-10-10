9290c33f80d84635b6d882b6fb8791ef0734c8d8
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2016-10-10 18:29:36 +1100

    lets do it again

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
-# from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
 from whitenoise.django import DjangoWhiteNoise
