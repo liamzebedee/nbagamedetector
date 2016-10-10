dac99b88ba0d4fe8b6bd5dbcee2ab91ac3d0c229
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2016-10-10 16:11:20 +1100

    fix deploy

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -15,8 +15,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
 # application = get_wsgi_application()
 
-from django.core.wsgi import get_wsgi_application
-# from whitenoise.django import DjangoWhiteNoise
+# from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-application = get_wsgi_application()
-# application = DjangoWhiteNoise(application)
+# application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
